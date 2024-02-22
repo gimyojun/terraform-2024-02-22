bdff70e3f1cc795b598a0a1b5bfdbc9acb36e545
gimyojun <gywnsdl5623@gmail.com>
2024-02-23 00:37:28 +0900

    ec2역할을 생성하고 역할에 정책을 부여. ec2에 iam역할을 연결

--- main.tf
+++ main.tf
@@ -64,6 +64,8 @@ resource "aws_subnet" "subnet_2" {
   }
 }
 
+
+
 resource "aws_internet_gateway" "igw_1" {
   vpc_id = aws_vpc.vpc_1.id
   tags = {
@@ -96,6 +98,7 @@ resource "aws_route_table_association" "association_2" {
   route_table_id = aws_route_table.rt_1.id
 }
 
+
 resource "aws_security_group" "sg_1" {
   name = "${var.prefix}-sg-1"
   // 인바운드 트래픽 규칙을 설정
@@ -120,5 +123,92 @@ resource "aws_security_group" "sg_1" {
 
   tags = {
     Name = "${var.prefix}-sg-1"
+  }
+}
+
+
+# EC2 설정 시작
+
+# EC2 역할 생성
+resource "aws_iam_role" "ec2_role_1" {
+  name = "${var.prefix}-ec2-role-1"
+
+  # 이 역할에 대한 신뢰 정책 설정. EC2 서비스가 이 역할을 가정할 수 있도록 설정
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
+
+# EC2 역할에 AmazonS3FullAccess 정책을 부착
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.ec2_role_1.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# EC2 역할에 AmazonEC2RoleforSSM 정책을 부착
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2_role_1.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+# IAM 인스턴스 프로파일 생성
+resource "aws_iam_instance_profile" "instance_profile_1" {
+  name = "${var.prefix}-instance-profile-1"
+  role = aws_iam_role.ec2_role_1.name
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_1" {
+  # 사용할 AMI ID
+  ami                         = "ami-07eff2bc4837a9e01"
+  # EC2 인스턴스 유형
+  instance_type               = "t2.micro"
+  # 사용할 서브넷 ID
+  subnet_id                   = aws_subnet.subnet_1.id
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids      = [aws_security_group.sg_1.id]
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_1.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-1"
+  }
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_2" {
+  # 사용할 AMI ID
+  ami                         = "ami-07eff2bc4837a9e01"
+  # EC2 인스턴스 유형
+  instance_type               = "t2.micro"
+  # 사용할 서브넷 ID
+  subnet_id                   = aws_subnet.subnet_2.id
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids      = [aws_security_group.sg_1.id]
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_1.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-2"
   }
 }
